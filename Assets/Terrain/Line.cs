a50e69160faaf79914799de94618acee01446453
Indrek Kaine <indrek.kaine@codelight.eu>
2018-03-16 22:47:17 +0200

    Add mouse line drag and raise terrain beneath it

---
+++ Assets/Terrain/Line.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+    //Draw line vars
+    List<Vector3> points;
+    public LineRenderer lineRenderer;
+    /*public EdgeCollider2D edgeCol;*/
+
+    public void updateLine(Vector3 mousePos)
+    {
+        if (points == null)
+        {
+            points = new List<Vector3>();
+            setPoint(mousePos);
+            return;
+        }
+
+        //Check if the mouse has moved enough for us tp insert new point.
+        if (Vector3.Distance(points.Last(), mousePos) > .1f)
+        {
+            setPoint(mousePos);
+        }
+    }
+
+    //Add the points to edgeCollider
+  void setPoint(Vector3 point)
+     {
+         points.Add(point);
+
+         lineRenderer.positionCount = points.Count;
+         lineRenderer.SetPosition(points.Count - 1, point);
+
+         //Add the points to edgeCollider
+         if (points.Count > 1)
+             points.ToArray();
+        //edgeCol.points = points.ToArray();
+    }
+}
---
+++ Assets/Terrain/LineCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour {
+
+    public GameObject linePrefab;
+    Line activeLine;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject lineGo = Instantiate(linePrefab);
+            activeLine = lineGo.GetComponent<Line>();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if(activeLine != null)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.updateLine(mousePos);
+        }
+	}
+}
